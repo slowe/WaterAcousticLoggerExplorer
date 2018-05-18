cb1a2a6302d1abaa421435f5dec044ae195b56b4
Stuart <gravatar@strudel.org.uk>
2018-05-18 14:59:51 +0100

    Add load function
    It doesn't do much yet.

--- resources/acousticlogger.js
+++ resources/acousticlogger.js
@@ -6,5 +6,67 @@
 */
 function AcousticLogger(attr) {
 
+	this.files = attr.files;
+	this.callback = {};
+	
 
+	return this;
+}
+
+Acoustic.prototype.log = function(){
+	if(this.logging){
+		var args = Array.prototype.slice.call(arguments, 0);
+		if(console && typeof console.log==="function") console.log('GWViewer',args);
+	}
+	return this;
+}
+
+Acoustic.prototype.load = function(){
+
+	if(!this.files) return this;
+
+	this.log('loading',this.files)
+	S().ajax(this.files.acousticData,{
+		'dataType': 'text',
+		'this':this,
+		'success': function(data,attrs){
+			this.log('complete',attrs,this);
+			
+			
+			
+			
+			
+			if(typeof this.callback.onload==="function") this.callback.onload.call(this);
+		},
+		'progress': function(e,attrs){
+			this.log('progress',attrs,this);
+			
+			
+			
+			if(typeof this.callback.onprogress==="function") this.callback.onprogress.call(this);
+		}
+	});
+
+	S().ajax(this.files.siteVisitData,{
+		'dataType': 'text',
+		'this':this,
+		'success': function(data,attrs){
+			this.log('complete',attrs,this);
+			
+			
+			
+			
+			
+			if(typeof this.callback.onload==="function") this.callback.onload.call(this);
+		},
+		'progress': function(e,attrs){
+			this.log('progress',attrs,this);
+			
+			
+			
+			if(typeof this.callback.onprogress==="function") this.callback.onprogress.call(this);
+		}
+	});
+
+	return this;
 }
